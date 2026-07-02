e6d7129d19f8e10b2dd1f799cd59020990603135
khantkyaw54 <khantkyaw7291@gmail.com>
2026-07-02 13:48:54 +0900

    Enhance layout and styles for hero, concept, and menu sections; add new assets and implement GSAP animations

--- Belo/src/main.js
+++ Belo/src/main.js
@@ -1,2 +1,27 @@
-// main.js
 import "./sass/style.scss";
+import gsap from "gsap";
+
+const header = document.querySelector(".header");
+const hero = document.querySelector(".hero");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > hero.offsetHeight - header.offsetHeight) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
+gsap.from(".hero__info", {
+    scrollTrigger: {
+        trigger: ".hero__info",
+        start: "top bottom",
+    },
+    y: 200,
+    opacity: 0,
+    duration: 1,
+    ease: "elastic.out(1)",
+    repeat: 1,
+    scale: 1.5,
+
+});
